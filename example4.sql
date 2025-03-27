3b929442306d3a22ce85c38c973b7e3fbcd80d47
kunalpatel77 <kunalpatel741559@gmail.com>
2025-03-28 01:44:05 +0530

    SQL file example 4

---
+++ example4.sql
@@ -0,0 +1,33 @@
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR,
+    Description TEXT,
+    Price DECIMAL,
+    CategoryID INT
+	)
+
+	select * from Products
+	INSERT INTO Products (ProductID, ProductName, Description, Price, CategoryID) VALUES
+(1, 'Laptop', 'High-performance laptop', 1200.00, 1),
+(2, 'Smartphone', 'Latest smartphone model', 800.00, 2),
+(3, 'Tablet', '10-inch tablet', 300.00, 2),
+(4, 'Headphones', 'Noise-canceling headphones', 150.00, 3),
+(5, 'Keyboard', 'Mechanical keyboard', 100.00, 3),
+(6, 'earbuds', 'noise-canceling', 100.00, 3),
+(7, 'Laptop', 'High-performance laptop', 11200.00, 4),
+(8, 'Smartphone', 'Latest smartphone model', 8700.00, 1),
+(9, 'Tablet', '10-inch tablet', 1300.00, 4),
+(10, 'Headphones', 'Noise-canceling headphones', 50.00, 1),
+(11, 'Keyboard', 'Mechanical keyboard', 1010.00, 5),
+(12, 'earbuds', 'noise-canceling', 1100.00, 2),
+(13, 'Laptop', 'High-performance laptop', 11200.00, 3),
+(14, 'Smartphone', 'Latest smartphone model', 8100.00, 3),
+(15, 'Tablet', '10-inch tablet', 1300.00, 5),
+(16, 'Headphones', 'Noise-canceling headphones', 1150.00, 4),
+(17, 'Keyboard', 'Mechanical keyboard', 1100.00, 6),
+(18, 'earbuds', 'noise-canceling', 1400.00, 4),
+(19, 'earbuds', 'noise-canceling', 6100.00, 2),
+(20, 'Laptop', 'High-performance laptop', 51200.00, 3)
+select * from Products
+
+
