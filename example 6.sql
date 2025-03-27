c60e843384d921ed504c894e4f54209880ee2ab6
kunalpatel77 <kunalpatel741559@gmail.com>
2025-03-28 02:27:00 +0530

    SQL file example 6

---
+++ example 6.sql
@@ -0,0 +1,32 @@
+create table student_performace(
+id bigint,
+Gender varchar,
+age bigint,
+city varchar,
+CGPA decimal
+)
+
+select * from student_performace
+
+insert into student_performace(id,Gender,age,city,CGPA) values
+(1,'Male',33,'Visakhapatnam',9.21),
+(2,'female',21,'pune',8.11),
+(3,'female',23,'mumbai',7.23),
+(4,'Male',26,'nagpur',7.71),
+(5,'female',28,'higna',9.21),
+(6,'Male',25,'patala',8.41),
+(7,'Male',31,'patna',8.21),
+(8,'female',43,'narendra nagar',6.21),
+(9,'female',22,'delhi',9.21),
+(10,'female',34,'surat',9.81),
+(11,'female',23,'seoni',8.11),
+(12,'female',22,'jabalpur',6.21),
+(13,'Male',28,'kuchna',9.11),
+(14,'Male',27,'majri',9.61),
+(15,'female',22,'vrindavan',7.11),
+(16,'Male',23,'warda',8.21),
+(17,'female',38,'banglore',7.21),
+(18,'Male',34,'kolkata',8.01),
+(19,'female',23,'agra',9.11),
+(20,'Male',33,'bhopal',9.41)
+select * from student_performace
