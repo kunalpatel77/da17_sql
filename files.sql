970e4246bb34c3d1c01a84d343726a5125d3f9de
kunalpatel77 <kunalpatel741559@gmail.com>
2025-03-28 00:06:18 +0530

    SQL file Example 1

---
+++ files.sql
@@ -0,0 +1,29 @@
+create table student (
+id bigint,
+name varchar,
+isActive boolean,
+mark bigint,
+subject varchar
+)
+
+select * from student
+
+insert into student (id,name,isActive,mark,subject) values (1,'kunal',true,92,'english')
+
+select * from student
+
+insert into student (id,name,isActive,mark,subject) values (2,'altamash',true,70,'english')
+
+select * from student
+insert into student (id,name,isActive,mark,subject) values ('3','singh',true,80,'english')
+
+insert into student (id,isActive,mark,subject) values (4,true,67,'english')
+
+insert into student (id,name,isActive,mark,subject) values 
+(5,'ajay',true,88,'english'),
+(6,'rahul',false,78,'english')
+select * from student
+
+
+
+
