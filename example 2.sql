4603b85936a9d9f9c3ed92c2c7ba633598c04d1b
kunalpatel77 <kunalpatel741559@gmail.com>
2025-03-28 01:14:57 +0530

    SQL 2nd example

---
+++ example 2.sql
@@ -0,0 +1,32 @@
+create table fees_status(
+rollno bigint,
+name varchar,
+gender varchar,
+feesstatus varchar
+)
+
+select * from fees_status
+insert into fees_status(rollno,name,gender,feesstatus) values 
+(1,'kunal','male','paid'),
+(2,'altamash','male','paid'),
+(3,'shruti','female','paid'),
+(4,'vinayak','male','unpaid'),
+(5,'sanskruti','female','paid'),
+(6,'manasvi','female','unpaid'),
+(7,'ajay','male','paid'),
+(8,'shantanu','male','unpaid'),
+(9,'abhishek','male','paid'),
+(10,'bhumika','female','unpaid'),
+(11,'sonal','female','unpaid'),
+(12,'vishal','male','unpaid'),
+(13,'sumit','male','paid'),
+(14,'roshni','female','paid'),
+(15,'rohan','male','paid'),
+(16,'pratik','male','paid'),
+(17,'pratiksha','female','unpaid'),
+(18,'naina','female','unpaid'),
+(19,'vaibhav','male','paid'),
+(20,'rohit','male','paid')
+select * from fees_status
+
+
